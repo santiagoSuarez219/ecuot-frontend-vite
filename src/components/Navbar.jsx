8cf03d26590bb750a98d5e3a1b09d313d490e2ce
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-12 11:21:05 -0500

    Corregir errores de formato y agregar modal en la barra de navegación

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { HiBars3 } from "react-icons/hi2";
 import { HiOutlineXMark } from "react-icons/hi2";
 import useEcuot from "../hooks/useEcuot";
+import Modal from "../modal";
 
 export default function Navbar() {
   const { modalNavbar, handleCloseModal, handleOpenModal } = useEcuot();
@@ -10,23 +11,23 @@ export default function Navbar() {
       <nav className="w-full flex p-4 lg:p-6 items-center justify-between">
         <div className="flex items-center">
           <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl tracking-tighter lg:mr-8">
-            <Link href={"/"}>ECUOT Repositorio Institucional</Link>
+            <Link to={"/"}>ECUOT Repositorio Institucional</Link>
           </h1>
           <ul className="hidden lg:flex gap-6 text-sm font-medium">
-            <li className="hover:border-b-2">
-              <Link href={"/noticias"}>Noticias</Link>
+            <li>
+              <Link to={"/noticias"}>Noticias</Link>
             </li>
-            <li className="hover:border-b-2">
-              <Link href={"/conflictos"}>Conflictos</Link>
+            <li>
+              <Link to={"/conflictos"}>Conflictos</Link>
             </li>
 
-            <li className="hover:border-b-2">
-              <Link href={"/intervenciones"}>Intervenciones</Link>
+            <li>
+              <Link to={"/intervenciones"}>Intervenciones</Link>
             </li>
           </ul>
         </div>
         <button className="hidden lg:block p-2 border border-gray-400 rounded-lg hover:bg-gray-200 transition-colors">
-          <Link href="/login">Iniciar sesion</Link>
+          <Link to="/login">Iniciar sesion</Link>
         </button>
         {modalNavbar ? (
           <HiOutlineXMark
@@ -38,6 +39,25 @@ export default function Navbar() {
             className="text-5xl z-10 lg:hidden"
             onClick={handleOpenModal}
           />
+        )}
+        {modalNavbar && (
+          <Modal>
+            <div className="w-4/5 h-[calc(100vh-96px)] bg-white border-t border-l border-black absolute bottom-0 right-0 p-4">
+              <ul className="flex flex-col gap-2 font-semibold">
+                <li onClick={handleCloseModal}>
+                  <Link to={"/noticias"}>Noticias</Link>
+                </li>
+                <li onClick={handleCloseModal}>
+                  <Link to={"/conflictos"}>Conflictos</Link>
+                </li>
+                <li onClick={handleCloseModal}>
+                  <Link to={"/intervenciones"}>Intervenciones</Link>
+                </li>
+                <div className="w-full h-[1px] rounded-sm bg-slate-200"></div>
+                <li>Iniciar sesion</li>
+              </ul>
+            </div>
+          </Modal>
         )}
       </nav>
     </>
