757e8d8d78f9e790ae367e33773f9e0e215bf30e
santiagoSuarez219 <santiago8628@gmail.com>
2024-03-23 09:44:12 -0500

    Agrega Navbar y sección de contenido en RutaProtegida

--- src/layouts/RutaProtegida.jsx
+++ src/layouts/RutaProtegida.jsx
@@ -1,9 +1,23 @@
 import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Navbar from "../components/Navbar";
 
 const RutaProtegida = () => {
   const { auth, cargando } = useAuth();
   if (cargando) return "Cargando...";
-  return <>{auth._id ? <Outlet /> : <Navigate to="/" />}</>;
+  return (
+    <>
+      {auth._id ? (
+        <>
+          <Navbar />
+          <section className="px-4">
+            <Outlet />
+          </section>
+        </>
+      ) : (
+        <Navigate to="/" />
+      )}
+    </>
+  );
 };
 export default RutaProtegida;
